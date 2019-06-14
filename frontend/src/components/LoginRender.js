8d56b931f71313ebe04520101178a9c25230bdfa
yoshonabee <jhann0120@gmail.com>
2019-06-15 02:00:11 +0800

    login query test sucdeeded

--- frontend/src/components/LoginRender.js
+++ frontend/src/components/LoginRender.js
@@ -9,9 +9,15 @@ import {
 import "../css/style.css" 
 import { BrowserRouter } from 'react-router-dom'
 export default class LoginRender extends Component {
+  state = {
+    login_button_on: false,
+    account: '',
+    password: ''
+  }
+
   render(){
+      console.log(this.state.login_button_on)
       return(
-          
         <div id="fh5co-contact">
         <div class="container">
           <div class="row animate-box">
@@ -20,29 +26,42 @@ export default class LoginRender extends Component {
               <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia.</p>
             </div>
           </div>
+
+          {this.state.login_button_on &&
+            <Query query={LOGIN_QUERY} variables={{ account: this.state.account, password: this.state.password }}>
+              {({ loading, error, data, subscribeToMore }) => {
+                if (!loading && !error) {
+                  this.props.login_action_handler(data.users[0].id)
+                  return <Redirect push to="/projects" />
+                }
+
+                return <div></div>
+              }}
+            </Query>
+          }
+
         </div>
         <div class="container">
             <div class="row">
               <div class="col-md-3 col-md-push-1 animate-box">
-              
                 
               </div>
               <div class="col-md-7 col-md-push-1 animate-box">
                 <div class="row">
                   <div class="col-md-10">
                     <div class="form-group">
-                      <input type="text" class="form-control" placeholder="Account Name"/>
+                      <input type="text" class="form-control" placeholder="Account Name" value={this.state.account} onChange={e => this.setState({account: e.target.value})} />
                     </div>
                   </div>
                   <div class="col-md-10">
                     <div class="form-group">
-                      <input type="text" class="form-control" placeholder="Password"/>
+                      <input type="text" class="form-control" placeholder="Password"  value={this.state.password} onChange={e => this.setState({password: e.target.value})}/>
                     </div>
                   </div>
 
                   <div class="col-md-6">
                     <div class="form-group">
-                      <input type="submit" value="login" class="btn btn-primary btn-lg btn-demo"/>
+                      <input type="submit" value="login" class="btn btn-primary btn-lg btn-demo" onClick={this.handleLoginButton}/>
                     </div>
                   </div>
                   <div class="col-md-6">
@@ -56,6 +75,12 @@ export default class LoginRender extends Component {
           </div>
       </div>
       )
+  }
+
+  handleLoginButton = e => {
+    if (this.state.account !== '' && this.state.password !== '') {
+      this.setState({login_button_on: true})
+    }
   }
 
 }
--- frontend/src/components/ProjectsRender.js
+++ frontend/src/components/ProjectsRender.js
@@ -11,6 +11,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 export default class ProjectsRender extends Component {
   render(){
+  	console.log(this.props.user_id)
       return(
 		<div id="fh5co-portfolio">
 		<div class="container">
