f9c723abbf4dc72e91061df582d976cd11ffd757
Byron.lee <byron.lee@grindr.com>
2019-06-14 15:40:12 +0800

    finished four filter and basic filter

--- basic_filter/src/containers/Home.js
+++ basic_filter/src/containers/Home.js
@@ -12,15 +12,15 @@ class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      blur: ["blur", NaN, "px"],
-      brightness: ["brightness", NaN, "%"],
-      contrast: ["contrast", NaN, "%"],
-      grayscale: ["grayscale", NaN, "%"],
+      blur: ["blur", 0, "px"],
+      brightness: ["brightness", 1, ""],
+      contrast: ["contrast", NaN, ""],
+      grayscale: ["grayscale", NaN, ""],
       hue_rotate: ["hue-rotate", NaN, "deg"],
-      invert: ["invert", NaN, "%"],
-      opacity: ["opacity", NaN, "%"],
-      saturate: ["saturate", NaN, "%"],
-      sepia: ["sepia", NaN, "%"],
+      invert: ["invert", NaN, ""],
+      opacity: ["opacity", NaN, ""],
+      saturate: ["saturate", NaN, ""],
+      sepia: ["sepia", NaN, ""],
     }
 
     this.applyFilter = this.applyFilter.bind(this);
@@ -33,32 +33,32 @@ class Home extends Component {
   initFilter() {
     this.setState({
       blur: ["blur", NaN, "px"],
-      brightness: ["brightness", NaN, "%"],
-      contrast: ["contrast", NaN, "%"],
-      grayscale: ["grayscale", NaN, "%"],
+      brightness: ["brightness", NaN, ""],
+      contrast: ["contrast", NaN, ""],
+      grayscale: ["grayscale", NaN, ""],
       hue_rotate: ["hue-rotate", NaN, "deg"],
-      invert: ["invert", NaN, "%"],
-      opacity: ["opacity", NaN, "%"],
-      saturate: ["saturate", NaN, "%"],
-      sepia: ["sepia", NaN, "%"],
-    });
+      invert: ["invert", NaN, ""],
+      opacity: ["opacity", NaN, ""],
+      saturate: ["saturate", NaN, ""],
+      sepia: ["sepia", NaN, ""],
+    }, this.useStateOnimage);
   }
   getClearFilter() {
     return {
       blur: ["blur", NaN, "px"],
-      brightness: ["brightness", NaN, "%"],
-      contrast: ["contrast", NaN, "%"],
-      grayscale: ["grayscale", NaN, "%"],
+      brightness: ["brightness", NaN, ""],
+      contrast: ["contrast", NaN, ""],
+      grayscale: ["grayscale", NaN, ""],
       hue_rotate: ["hue-rotate", NaN, "deg"],
-      invert: ["invert", NaN, "%"],
-      opacity: ["opacity", NaN, "%"],
-      saturate: ["saturate", NaN, "%"],
-      sepia: ["sepia", NaN, "%"],
+      invert: ["invert", NaN, ""],
+      opacity: ["opacity", NaN, ""],
+      saturate: ["saturate", NaN, ""],
+      sepia: ["sepia", NaN, ""],
     }
   }
 
   setStateWithEvent(e) {
-    console.log(e.target.name, this.state[e.target.name])
+    // console.log(e.target.name, this.state[e.target.name])
     var tmp = this.state[e.target.name]
     tmp[1] = e.target.value
     var obj = {}
@@ -110,10 +110,9 @@ class Home extends Component {
   parseFIlterCss(e, css) {
     let cssArr = css.split(" ")
     var obj = this.getClearFilter()
-    console.log(obj)
     cssArr.forEach(function(element) {
       var name = element.split("(")[0]
-      if (name == "hue-rotate") {
+      if (name === "hue-rotate") {
         name = "hue_rotate"
       }
       var value = element.split("(")[1].slice(0, -1);
@@ -121,13 +120,12 @@ class Home extends Component {
       tmp[1] = value
       obj[name] = tmp
     });
-    this.setState(obj)
-    this.useStateOnimage()
+    // console.log(obj)
+    this.setState(obj, this.useStateOnimage)
   }
 
   resetFilter() {
     this.initFilter()
-    this.useStateOnimage()
   }
 
   render() {
@@ -140,40 +138,40 @@ class Home extends Component {
           </div>
 
           <BasicInput
-            name="blur" min="0" max="10" value="0" step="0.1"
+            name="blur" min="0" max="10" value={this.state.blur[1]} step="0.1"
             datafilter="blur" datascale="px" onChange={(e) => this.applyFilter(e)}
           />
           <BasicInput
-            name="brightness" min="0" max="200" value="100" step="1"
-            datafilter="brightness" datascale="%" onChange={(e) => this.applyFilter(e)}
+            name="brightness" min="0" max="2" value={this.state.brightness[1]} step="0.01"
+            datafilter="brightness" datascale="" onChange={(e) => this.applyFilter(e)}
           />
           <BasicInput
-            name="contrast" min="0" max="200" value="100" step="1"
-            datafilter="contrast" datascale="%" onChange={(e) => this.applyFilter(e)}
+            name="contrast" min="0" max="2" value="1" step="0.01"
+            datafilter="contrast" datascale="" onChange={(e) => this.applyFilter(e)}
           />
           <BasicInput
-            name="grayscale" min="0" max="100" value="0" step="1"
-            datafilter="grayscale" datascale="%" onChange={(e) => this.applyFilter(e)}
+            name="grayscale" min="0" max="1" value="0" step="0.01"
+            datafilter="grayscale" datascale="" onChange={(e) => this.applyFilter(e)}
           />
           <BasicInput
             name="hue_rotate" min="-180" max="180" value="0" step="1"
             datafilter="hue-rotate" datascale="deg" onChange={(e) => this.applyFilter(e)}
           />
           <BasicInput
-            name="invert" min="0" max="100" value="0" step="1"
-            datafilter="invert" datascale="%" onChange={(e) => this.applyFilter(e)}
+            name="invert" min="0" max="1" value="0" step="0.01"
+            datafilter="invert" datascale="" onChange={(e) => this.applyFilter(e)}
           />
           <BasicInput
-            name="opacity" min="0" max="100" value="100" step="1"
-            datafilter="opacity" datascale="%" onChange={(e) => this.applyFilter(e)}
+            name="opacity" min="0" max="1" value="1" step="0.01"
+            datafilter="opacity" datascale="" onChange={(e) => this.applyFilter(e)}
           />
           <BasicInput
-            name="saturate" min="0" max="100" value="100" step="1"
-            datafilter="saturate" datascale="%" onChange={(e) => this.applyFilter(e)}
+            name="saturate" min="0" max="1" value="1" step="0.01"
+            datafilter="saturate" datascale="" onChange={(e) => this.applyFilter(e)}
           />
           <BasicInput
-            name="sepia" min="0" max="100" value="0" step="1"
-            datafilter="sepia" datascale="%" onChange={(e) => this.applyFilter(e)}
+            name="sepia" min="0" max="1" value="0" step="0.01"
+            datafilter="sepia" datascale="" onChange={(e) => this.applyFilter(e)}
           />
 
           <br />
@@ -182,9 +180,16 @@ class Home extends Component {
 
           <h3>Style Button</h3>
           <button onClick={() => this.resetFilter()}>Origin</button>
-          <button onClick={(e) => this.parseFIlterCss(e, "sepia(50) hue-rotate(-30) saturate(140)")}>1977</button>
-          <button onClick={(e) => this.parseFIlterCss(e, "sepia(20) brightness(115) saturate(140)")}>aden</button>
-          <button onClick={(e) => this.parseFIlterCss(e, "sepia(35) contrast(110) brightness(120) saturate(130)")}>amaro</button>
+          <button onClick={(e) => this.parseFIlterCss(e, "sepia(.5) hue-rotate(-30) saturate(1.40)")}>1977</button>
+          <button onClick={(e) => this.parseFIlterCss(e, "sepia(.2) brightness(1.15) saturate(1.4)")}>aden</button>
+          <button onClick={(e) => this.parseFIlterCss(e, "sepia(.35) contrast(1.1) brightness(1.2) saturate(1.3)")}>amaro</button>
+          <button onClick={(e) => this.parseFIlterCss(e, "sepia(.25) contrast(1.25) brightness(1.15) saturate(.9) hue-rotate(-5)")}>earlybird</button>
+          <button onClick={(e) => this.parseFIlterCss(e, "brightness(1.4) contrast(.95) saturate(0) sepia(.35)")}>moon</button>
+          <button onClick={(e) => this.parseFIlterCss(e, "sepia(.25) contrast(1.5) brightness(.95) hue-rotate(-15)")}>toaster</button>
+          <button onClick={(e) => this.parseFIlterCss(e, "sepia(.45) contrast(1.25) brightness(1.75) saturate(1.3) hue-rotate(-5)")}>xpro-ii</button>
+          <button onClick={(e) => this.parseFIlterCss(e, "sepia(.15) contrast(1.25) brightness(1.25) hue-rotate(5)")}>clarendon</button>
+          <button onClick={(e) => this.parseFIlterCss(e, "sepia(.25) contrast(1.25) brightness(1.2) saturate(.9)")}>rise</button>
+
           <br />
           <br />
           <br />
